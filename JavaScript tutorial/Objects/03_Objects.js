c6d6952a77a94cee4fa3b161e60978a0f8fc069d
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-09-24 23:11:08 +0530

    Objescts De-Structuring

---
+++ JavaScript tutorial/Objects/03_Objects.js
@@ -0,0 +1,22 @@
+// De-Structuring
+
+const course={
+    coursename:"jis in hindi",
+    price:"999",
+    courseInstructor:"Hitesh Chaudhary"
+}
+
+// course.courseInstructor
+
+const{courseInstructor:inst}=course
+console.log(inst);
+
+
+// ++++++++++++++++++++++++++++    API    ++++++++++++++++++++++++++++//
+
+// {
+//     "name":"hitesh",
+//     "courseName" :"java Script",
+//     "price":"free"
+
+// }
