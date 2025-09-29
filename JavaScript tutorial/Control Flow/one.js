9b1733360cf6e6373cf607bf7e33aa22d7e83e34
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-09-29 23:24:58 +0530

    basic control statements

---
+++ JavaScript tutorial/Control Flow/one.js
@@ -0,0 +1,32 @@
+//  if
+// const isUserLoggedIn=true
+// if (isUserLoggedIn) {
+//     console.log("User is logged in")
+    
+// }
+
+// const score=200
+// if (score>100) {
+//     console.log("User has good score")
+// }
+// else{
+//     console.log("User has bad score")
+// }
+
+
+const balance=1000
+// if (balance>500) log("User has enough balance"),
+// console.log("User has no enough balance");
+
+if (balance>500) {
+    console.log("User has enough balance")
+}
+else if (balance<750) {
+    console.log("User has medium balance")
+}
+else if (balance<1000) {
+    console.log("User has low balance")
+}
+else{
+    console.log("User has no enough balance")
+}
