6e1c25d53bcf23594f43bb23cbf3dcadb6a61a01
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-10-01 22:48:47 +0530

    loops

---
+++ JavaScript tutorial/Loop/02_whileLoop.js
@@ -0,0 +1,22 @@
+let i = 0;
+while (i <= 10) {
+    console.log(`Value of i is ${i}`);
+    i += 2;
+}
+
+let myArr = ["flash", "batman", "Superman"]
+let arr = 0;
+while (arr < myArr.length) {
+    console.log(`value is ${myArr[arr]}`);
+    arr += 1;
+
+}
+
+
+let score=11
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while (score<=10)
+
+    
